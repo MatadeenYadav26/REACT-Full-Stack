b8832d05b23d0287173177f6287fb2e404b5e58d
Matadeen Yadav <matadeenyadav202@gmail.com>
2026-05-24 06:03:06 +0530

    Add files via upload

---
+++ 1.Day-66_Basics_Before_REACT/app.js
@@ -0,0 +1,32 @@
+// // 2 Ways to export:
+// // - Default export : har file 1 hi default export kar skti hai  , import karte time kisi v name se impoert kar skte hai
+
+// // default export:
+
+// // var user = 'Aman'
+// // var skills = ['react','js','jsx']
+
+// // export default user
+// // export default skills // error as a module cannot have multiple default exports
+
+
+// const king = {
+//     name:"Virat Kohli",
+//     age:37,
+//     century:85,
+//     isForm:true
+// }
+
+// // const queen = {
+// //     name:"Anushka Sharma",
+// //     age:35,
+// //     movies:39,
+// //     isHit:true
+// // }
+
+// export default king // default export
+
+
+// // named export : ek file me jitne banana chaho bana skte ho!
+
+// export const user = 'Aman' // named export
---
+++ 1.Day-66_Basics_Before_REACT/test.js
@@ -0,0 +1,23 @@
+// const skills = ['JS','REACT','Next']
+
+// export const math = 90;
+// export const chem = 85;
+// export const phy = 95;
+
+// export default skills
+
+
+export const Buthcer = {
+    name:"Billy-Butcher",
+    age:32,
+    inForm:true
+}
+
+const Ghar = {
+    name:"HomeLander",
+    age:35,
+    inForm:false
+}
+
+
+export default Ghar;
