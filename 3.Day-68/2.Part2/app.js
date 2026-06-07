48e53303a03edddd92494870171639bc0ddd7d0e
Matadeen Yadav <matadeenyadav202@gmail.com>
2026-06-07 19:33:52 +0530

    Add files via upload

---
+++ 3.Day-68/2.Part2/app.js
@@ -0,0 +1,5 @@
+const app = ()=>{
+    return React.createElement('h1',null,'This is App')
+}
+
+export default app
---
+++ 3.Day-68/2.Part2/main.js
@@ -0,0 +1,6 @@
+import app from './app.js'
+
+
+const root = ReactDOM.createRoot(document.querySelector('#container'))
+
+root.render(app())
