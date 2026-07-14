d4362d66d9fd80fb962742f165c6441708c5d2f9
Matadeen Yadav <matadeenyadav202@gmail.com>
2026-07-15 03:01:15 +0530

    Add files via upload

---
+++ 3.Day-68/src/App.jsx
@@ -0,0 +1,26 @@
+// const App = () =>{
+//   // return 'Hello from App'
+//   // return <h1>Hello </h1>
+//   return <div>
+//     <h1>Hey</h1>
+//     <h2>Hello</h2>
+//     <p>How are you?</p>
+//   </div>
+// }
+
+// export default App
+
+
+
+// rafce:
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello Human</h1>
+    </div>
+  )
+}
+
+export default App
