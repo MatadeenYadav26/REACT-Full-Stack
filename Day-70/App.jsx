af15bf2c8543d3e2a7a35dfdb2765e59898bf56c
Matadeen Yadav <matadeenyadav202@gmail.com>
2026-07-22 22:09:03 +0530

    Add files via upload

---
+++ Day-70/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div>
+      <h1 className='text-5xl bg-amber-500'>Hello ji Hello</h1>
+      <h2 className='text-3xl bg-emerald-800'>Hiiiii Subtitle</h2>
+    </div>
+  )
+}
+
+export default App
