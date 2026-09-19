be75c8ba46393bbad531bb633a301d8f7f100428
Matadeen Yadav <matadeenyadav202@gmail.com>
2026-09-19 10:43:56 +0530

    Add files via upload

---
+++ Day_83/Class_15/src/context/ThemeContext.jsx
@@ -0,0 +1,19 @@
+import React, { createContext } from 'react'
+import { useState } from 'react'
+export const ThemeDataContext = createContext()
+
+
+const ThemeContext = (props) => {
+
+    const [theme, setTheme] = useState('Ye Data Hai')
+
+  return (
+    <div>
+      <ThemeDataContext.Provider value={[theme, setTheme]}>
+        {props.children}
+      </ThemeDataContext.Provider>
+    </div>
+  )
+}
+
+export default ThemeContext
